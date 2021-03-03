c3811d86a7c0dc1149157d4d2607ca535e251aa8
unknown <luannm1410@gmail.com>
2021-03-03 00:55:09 -0600

    Updated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'resume-demo-app';
+  title = 'My Profile';
 
   private API_URL = 'http://localhost:8080/SpringMVCDemo';
   
